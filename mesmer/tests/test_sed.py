7455c346c767baa5b380b2a0e3a2a78c4ba13707
Ben Thorne <bn.thorne@googlemail.com>
2020-07-29 13:03:48 -0700

    Fixed typo

--- mesmer/tests/test_sed.py
+++ mesmer/tests/test_sed.py
@@ -1,6 +1,6 @@
 def test_sed():
     import numpy as np
-    from mesmer.seds import sync
-    arr = sync(np.linspace(10, 100), 23., - 3.)
+    from mesmer.seds import syncpl
+    arr = syncpl(np.linspace(10, 100), 23., - 3.)
     arr += 1.
     return True
